37c2dc4a90cfc388347e2e2310870f5b16ec8554
Ravi Krishnan U B <ravikuttoose07@gmail>
2025-04-19 18:31:31 +0530

    A lot of BUGS

--- vanilla-js-version/main.js
+++ vanilla-js-version/main.js
@@ -1,4 +1,4 @@
-
+let game_loop, object_creation_loop;
 function main(){
     // ✅
     function collission(hole_rows){
@@ -33,11 +33,13 @@ function main(){
     }
     // ✅
     function flappyFalling(){
-        const flappy_bird = document.getElementById('flappy-bird');
-        var flappy_bird_row = parseInt(getComputedStyle(flappy_bird).gridRowStart);
-        flappy_bird.style.gridRowStart = flappy_bird_row + 1;
-        if (flappy_bird.style.gridRowStart == rows){
-            gameOver()
+        if(is_game_over == false){
+            const flappy_bird = document.getElementById('flappy-bird');
+            var flappy_bird_row = parseInt(getComputedStyle(flappy_bird).gridRowStart);
+            flappy_bird.style.gridRowStart = flappy_bird_row + 1;
+            if (flappy_bird.style.gridRowStart == rows){
+                gameOver()
+            }
         }
     }
 
@@ -45,71 +47,79 @@ function main(){
     function moveObstacles(){
         // for each obstacle in obstacles, move left (gridColumnStart - 1, gridRowStart)
         // console.log('should move -1 to left');
-
-        let obstacles= document.getElementsByClassName('obstacle');
-        let holes = document.getElementsByClassName('hole');
+        if(is_game_over == false){
+            let obstacles= document.getElementsByClassName('obstacle');
+            let holes = document.getElementsByClassName('hole');
         
-        for(let i = 0; i < obstacles.length; i++){
-            // move obstacle
-            let obstacle_column = parseInt(getComputedStyle(obstacles[i]).gridColumnStart);
-            obstacles[i].style.gridColumnStart = obstacle_column - 1;
+            for(let i = 0; i < obstacles.length; i++){
+                // move obstacle
+                let obstacle_column = parseInt(getComputedStyle(obstacles[i]).gridColumnStart);
+                obstacles[i].style.gridColumnStart = obstacle_column - 1;
 
-            // move holes
-            let hole_column = parseInt(getComputedStyle(holes[i]).gridColumnStart);
-            holes[i].style.gridColumnStart = hole_column - 1;
+                // move holes
+                let hole_column = parseInt(getComputedStyle(holes[i]).gridColumnStart);
+                holes[i].style.gridColumnStart = hole_column - 1;
         }
+        }
+        
 
    
 
     }
     // ✅   
     function createObstacles(rows, columns, hole_size, hole_rows){
-        console.log(hole_rows);
-        
-        const game_board = document.getElementById('game-board');
-        // obstacle and hole creation
-        const new_obstacle =document.createElement('div');
-        new_obstacle.className = 'obstacle';
-        const new_hole = document.createElement('div');
-        new_hole.className = 'hole';
+        if(is_game_over == false){
+            // console.log(hole_rows);
+            
+            const game_board = document.getElementById('game-board');
+            // obstacle and hole creation
+            const new_obstacle =document.createElement('div');
+            new_obstacle.className = 'obstacle';
+            const new_hole = document.createElement('div');
+            new_hole.className = 'hole';
 
-        let hole_center = Math.floor(Math.random() * ((rows - 2) - 2) + 2)
+            let hole_center = Math.floor(Math.random() * ((rows - 2) - 2) + 2)
 
-        // obstacle style
-        new_obstacle.style.gridColumnStart = columns;
-        new_obstacle.style.gridRowStart = 1;
-        new_obstacle.style.gridRowEnd = rows + 1;
+            // obstacle style
+            new_obstacle.style.gridColumnStart = columns;
+            new_obstacle.style.gridRowStart = 1;
+            new_obstacle.style.gridRowEnd = rows + 1;
 
-        // hole style
-        new_hole.style.gridColumnStart = new_obstacle.style.gridColumnStart;
-        new_hole.style.gridRowStart = hole_center - hole_size/2;
-        new_hole.style.gridRowEnd = hole_center + hole_size/2 + 1;
+            // hole style
+            new_hole.style.gridColumnStart = new_obstacle.style.gridColumnStart;
+            new_hole.style.gridRowStart = hole_center - hole_size/2;
+            new_hole.style.gridRowEnd = hole_center + hole_size/2 + 1;
 
-        game_board.appendChild(new_obstacle);
-        game_board.appendChild(new_hole);
+            game_board.appendChild(new_obstacle);
+            game_board.appendChild(new_hole);
 
-        let new_hole_row = []
-        for(let i = -hole_size/2; i < hole_size/2 + 1; i++){
-            new_hole_row.push(hole_center + i);
+            let new_hole_row = []
+            for(let i = -hole_size/2; i < hole_size/2 + 1; i++){
+                new_hole_row.push(hole_center + i);
+            }
+
+            hole_rows.push(new_hole_row);
         }
-
-        hole_rows.push(new_hole_row);
+        
         
     }
     // ✅  
     function clearObstacles(rows, columns, hole_rows){
-        const game_board = document.getElementById('game-board');
-        const column_threshold = 1;
-        const obstacles = document.getElementsByClassName('obstacle');
-        const holes = document.getElementsByClassName('hole');
+        if(is_game_over == false){
+            const game_board = document.getElementById('game-board');
+            const column_threshold = 1;
+            const obstacles = document.getElementsByClassName('obstacle');
+            const holes = document.getElementsByClassName('hole');
 
-        for(let i = 0; i < obstacles.length; i++){
-            if(obstacles[i].style.gridColumnStart <= column_threshold){
-                game_board.removeChild(obstacles[i])
-                game_board.removeChild(holes[i])
-                hole_rows.shift();
+            for(let i = 0; i < obstacles.length; i++){
+                if(obstacles[i].style.gridColumnStart <= column_threshold){
+                    game_board.removeChild(obstacles[i])
+                    game_board.removeChild(holes[i])
+                    hole_rows.shift();
+                }
             }
         }
+        
     }
 
     function detectChange(rows, columns, hole_rows){
@@ -120,7 +130,9 @@ function main(){
             var flappy_bird_row = parseInt(getComputedStyle(flappy_bird).gridRowStart);
             var flappy_bird_col = parseInt(getComputedStyle(flappy_bird).gridColumnStart);    
             // console.log(hole_rows);
-            
+            if(is_game_over == true && event.code === 'Space'){
+                restartGame()
+            }
             if(is_game_over == false){
                 switch(key){
                     case 'ArrowUp':
@@ -130,10 +142,10 @@ function main(){
                         }
                         break;
                     case 'ArrowDown':
-                        flappy_bird.style.gridRowStart = Math.max(flappy_bird_row - 1, 0);
+                        flappy_bird.style.gridRowStart = Math.max(flappy_bird_row - 1, 1);
                         break;
                     case 'ArrowLeft':
-                        flappy_bird.style.gridColumnStart = Math.max(flappy_bird_col - 1, 0);
+                        flappy_bird.style.gridColumnStart = Math.max(flappy_bird_col - 1, 1);
                         break;
                     case 'ArrowRight':
                         flappy_bird.style.gridColumnStart = Math.min(flappy_bird_col + 1, columns);
@@ -168,14 +180,41 @@ function main(){
         clearInterval(object_creation_loop);
         console.log('overß');
         is_game_over = true;
+
         if(score.innerHTML > high_score.innerHTML){
             high_score.innerHTML = score.innerHTML;
         }
+        
+        
     }
+
+    function restartGame(){
+        const score = document.getElementById('current-score');
+        const game_over_overlay = document.getElementById('game-over');
+        const game_board = document.getElementById('game-board');
+        const obstacles = document.getElementsByClassName('obstacle');
+        const holes = document.getElementsByClassName('hole');
+
+
+        game_over_overlay.style.display = 'none';
+        score.innerHTML = 0;
+
+        for(let i = 0; i < obstacles.length; i++){
+            game_board.removeChild(obstacles[i]);
+            game_board.removeChild(holes[i]);
+        }
+        clearInterval(game_loop);
+        clearInterval(object_creation_loop);
+        main();
+    }
+    
 
     const board = document.getElementById("game-board");
     const columns = getComputedStyle(board).getPropertyValue("grid-template-columns").split(" ").length;
     const rows = getComputedStyle(board).getPropertyValue("grid-template-rows").split(" ").length;
+    const flappy = document.getElementById('flappy-bird');
+    flappy.style.gridRowStart = 10;
+    flappy.style.gridColumnStart = 5;
 
     let is_game_over = false;
     const hole_size = 4; // only even values
@@ -183,11 +222,11 @@ function main(){
     console.log("Columns:", columns);
     console.log("Rows:", rows);  
     let hole_rows = []
-    // var hole_rows = showObstacles(rows, columns, hole_size);
+
     detectChange(rows, columns, hole_rows);
 
     // game run loop
-    let game_loop = setInterval(()=>{
+    game_loop = setInterval(()=>{
         
         flappyFalling();
         moveObstacles();
@@ -196,9 +235,16 @@ function main(){
     }, 300)
 
     // object creation loop
-    let object_creation_loop = setInterval(()=>{
+    object_creation_loop = setInterval(()=>{
         createObstacles(rows, columns, hole_size, hole_rows);
-    }, 5000)
+    }, 5000);
+
+    document.addEventListener('keydown', (event)=>{
+        if(event.code === 'Space' & is_game_over & document.getElementById('game-over').style.display === 'block'){
+            restartGame();
+        }
+    })
+
 }
 
 main()
